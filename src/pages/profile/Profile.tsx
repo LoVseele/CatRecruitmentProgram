67033f40e06e96ac13444e039fea03565d139051
chen15015944299 <3490835164@qq.com>
2025-08-20 18:16:14 +0800

    feat:个人页面第一次传

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,10 +1,17 @@
 // pages/intro/Intro.tsx
-import { View, Text } from "@tarojs/components";
+import { View, Text} from "@tarojs/components";
 import "../index.scss";
+import "./profile.scss";
 
 const Intro = () => (
   <View className="page-content">
-    <Text className="page-title">这是个人页面</Text>
+    <image className="avatar"/>
+    <Text className="name">姓名:</Text>
+    <view className="list">
+      <view className="list-item list-item1">报名信息</view>
+      <view className="list-item list-item2">我的通知</view>
+      <view className="list-item list-item3">联系我们</view>
+    </view>
   </View>
 );
 
