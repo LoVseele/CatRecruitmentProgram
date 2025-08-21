e29a3e118b8213994a6b280ee83a74f7c7408492
LoVseele <1539637286@qq.com>
2025-08-21 16:13:40 +0800

    feat:介绍界面的简单功能实现

--- src/pages/intro/Intro.tsx
+++ src/pages/intro/Intro.tsx
@@ -1,11 +1,45 @@
 // pages/intro/Intro.tsx
-import { View, Text } from "@tarojs/components";
+import { View, Text, Swiper, SwiperItem, Image } from "@tarojs/components";
 import "../index.scss";
+import "./intro.scss";
+import { useState } from "react";
+import { blocks } from "./data";
+export default function Intro() {
+  const [current, setCurrent] = useState(0);
 
-const Intro = () => (
-  <View className="page-content">
-    <Text className="page-title">这是介绍页面</Text>
-  </View>
-);
-
-export default Intro;
+  return (
+    <View className="page-content">
+      <Swiper
+        className="intro"
+        vertical
+        duration={600} // 保持滑动动画时间
+        onChange={(e) => setCurrent(e.detail.current)}
+      >
+        {blocks.map((item, index) => (
+          <SwiperItem key={index}>
+            <View
+              className={`intro-item ${current === index ? "active" : ""}`}
+              style={{ backgroundColor: item.color }}
+            >
+              <View className="intro-item-title">{item.title}</View>
+              {item.image && (
+                <Image
+                  className="intro-item-image"
+                  src={item.image}
+                  mode="widthFix"
+                />
+              )}
+              <View>
+                {item.paragraphs.map((p, idx) => (
+                  <Text key={idx} className="intro-item-text">
+                    {p}
+                  </Text>
+                ))}
+              </View>
+            </View>
+          </SwiperItem>
+        ))}
+      </Swiper>
+    </View>
+  );
+}
