75a78eeb0e8918bff391e5b64cb5d0f1ab5ea1aa
FengZX <3325720582@qq.com>
2025-08-25 15:00:29 +0800

    feat:进度页面完善v1.0

--- src/pages/progress/Progress.tsx
+++ src/pages/progress/Progress.tsx
@@ -7,16 +7,17 @@ interface StageInfo {
   state: string | number; // 状态标识（可以是字符串或数字）
   text: string;          // 阶段名称/标题
   intro: string;         // 阶段简介
+  style: any;
 }
 
 // 本地预设的阶段数据
 const stageData: StageInfo[] = [
-  { state: "unRegister", text: "未报名", intro: "你还没报名哦, 快来报名加入C.A.T工作室吧" },
-  { state: "register", text: "报名完成", intro: "成功报名 ! 离进入C.A.T不远了" },
-  { state: "interview", text: "面试", intro: "面试时间到 ! 快来和师兄师姐们深入交流" },
-  { state: "finst", text: "第一轮考核", intro: "第一轮考核 , 考验你的基本功" },
-  { state: "second", text: "第二轮考核", intro: "第二轮考核 , 考验你的耐心和细心" },
-  { state: "offer", text: "成功录取", intro: "恭喜你成为C.A.T的成员 !" }
+  { state: "unRegister", text: "未报名", intro: "你还没报名哦, 快来报名加入C.A.T工作室吧", style: { display: 'none' } },
+  { state: "register", text: "报名完成", intro: "成功报名 ! 离进入C.A.T不远了", style: { bottom: '35%', left: '5%' } },
+  { state: "interview", text: "面试", intro: "面试时间到 ! 快来和师兄师姐们深入交流", style: { bottom: '41%', left: '21%' } },
+  { state: "finst", text: "第一轮考核", intro: "第一轮考核 , 考验你的基本功", style: { bottom: '58%', left: '36%' } },
+  { state: "second", text: "第二轮考核", intro: "第二轮考核 , 考验你的耐心和细心", style: { bottom: '71%', left: '51%' } },
+  { state: "offer", text: "成功录取", intro: "恭喜你成为C.A.T的成员 !", style: { bottom: '23%', left: '89%' } }
 ];
 
 const stage = stageData[0];
@@ -35,9 +36,14 @@ const Intro: FC<ProProps> = ({
       <Image
         src={proCat}
         className="proCat"
+        style={{
+          position: 'absolute' as const,
+          ...stage.style
+        }}
       ></Image>
     </View>
-    <View className="progress-content" onClick={onGoToRegistration}>
+    <View className="progress-content"
+      onClick={stage.state === 'unRegister' ? onGoToRegistration : undefined}>
       <Text>当前阶段: {stage.text}</Text>
       <Text>{stage.intro}</Text>
     </View>
