f5121b2b8c3adbde04ba374950cd00d3bb3d45a9
FengZX <3325720582@qq.com>
2025-08-27 18:06:31 +0800

    路径添加

--- src/app.config.ts
+++ src/app.config.ts
@@ -6,7 +6,8 @@ export default {
     "pages/profile/registration/registration",
     "pages/profile/notifications/notifications",
     "pages/profile/contact/contact",
-    "pages/progress/registration/registration"
+    "pages/progress/registration/registration",
+    "pages/progress/interview/interview"
   ], // 目标页面（添加这一行）
   window: {
     navigationStyle: "custom",
