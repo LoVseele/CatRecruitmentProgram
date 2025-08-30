d88477dd80917659dfa9d60c94daa4cb650c736a
FengZX <3325720582@qq.com>
2025-08-30 15:37:09 +0800

    Revert "Merge branch 'feat' of https://github.com/LoVseele/CatRecruitmentProgram into feat"
    This reverts commit 8019b018d8998131897f796b7da0545d2cebfc73, reversing
    changes made to 5db10da0da37ebd2f3234abd3eb6f4e502505e7d.

--- src/app.config.ts
+++ src/app.config.ts
@@ -3,12 +3,30 @@ export default {
   pages: [
     "pages/index/index",
     "pages/profile/ProLogic",
+    "pages/profile/registration/registration",
     "pages/profile/notifications/notifications",
     "pages/profile/contact/contact",
-    "pages/progress/interview/interview",
-    "pages/registration/registration",
+    "pages/progress/registration/registration",
+    "pages/progress/interview/interview"
   ], // 目标页面（添加这一行）
   window: {
     navigationStyle: "custom",
   },
+  // 关键：仅保留 pagePath 和 text，删除 iconPath/selectedIconPath
+  /*   tabBar: {
+    list: [
+      {
+        pagePath: "pages/intro/index",
+        text: "",
+      },
+      {
+        pagePath: "pages/progress/index",
+        text: "",
+      },
+      {
+        pagePath: "pages/profile/index",
+        text: "",
+      },
+    ],
+  }, */
 };
